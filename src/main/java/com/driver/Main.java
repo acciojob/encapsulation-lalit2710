7f4a9f37d1909a712d56b4f90ca37197d75ecfdb
lalit2710 <ltk143.lk@gmail.com>
2022-10-08 12:22:52 +0530

    Encapsulation done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+
+        RWOnly obj = new RWOnly();
+//        obj.name = "Lalit";
+        //System.out.println("name has private access in com.driver.RWOnly")
+       obj.setName("Lalit");
+        System.out.println(obj.getName());
+    }
   
 }
